16bd4d5c124f3f9c1bf6ff6747eb165d01655425
Sang Huynh Thanh <sanghuynh20000@gmail.com>
2024-04-13 14:23:55 +0700

    Add regex pattern for c9users.io to config hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,6 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+
+  config.hosts << /[a-z0-9]+\.c9users\.io/
 end
