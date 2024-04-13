9c448c92df980c68859cb9bde02ad9213b114a50
Sang Huynh Thanh <sanghuynh20000@gmail.com>
2024-04-13 14:28:03 +0700

    Update config hosts to use herokuapp.com domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,5 +103,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.hosts << /[a-z0-9]+\.c9users\.io/
+  config.hosts << /[a-z0-9]+\.herokuapp\.com/
 end
